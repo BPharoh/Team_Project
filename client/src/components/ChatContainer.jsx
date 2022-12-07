a3cbb4dc6527c4335004db7008064be2bb077d19
Otulugbu Alexander <s2200742@Alexander-MacBook-Air.local>
2022-12-07 19:33:45 +0200

    Commits: Updating main to branch alexander

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,2 +1,33 @@
+import React from "react";
+import Nav from "./Nav";
+import ChatsHeader from "./ChatsHeader";
+import MatchesDisplay from "./MatchesDisplay";
+import ChatsDisplay from "./ChatsDisplay";
+
+const ChatContainer = () => {
+  return (
+    <div className="chat-wrapper">
+      <Nav />
+      <div className="chat-headline">
+             <h1>Chat</h1> 
+        <div className="chat-container">
+            <div className="chat-profilesContainer">
+            <ChatsHeader  /> 
+            <div>
+                <button className="option">Matches</button>
+                <button className="option">Chat</button>
+            </div>    
+            <MatchesDisplay />
+            <ChatsDisplay />
+            </div>
+            <div className="chat-displayContainer">
+            Lorem ipsum 
+            </div>
+        </div>
+      </div>
+      </div>
+
+  );
+};
 
 export default ChatContainer;
